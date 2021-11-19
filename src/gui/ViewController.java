aefb1c1d2ad602814f1cb33477cce2ba9984ca15
Luan Araujo <luanaraujo9977@gmail.com>
2021-11-19 18:42:19 -0300

    Limitações para TextField e Initializable

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,13 +1,18 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable {
 	
 	@FXML
 	private TextField txtNumber1;
@@ -32,5 +37,13 @@ public class ViewController {
 		catch (NumberFormatException e) {
 			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber1, 12);
+		Constraints.setTextFieldMaxLength(txtNumber2, 12);
 	}
 }
